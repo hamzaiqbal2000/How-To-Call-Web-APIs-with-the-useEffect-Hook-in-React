5c7044e5569e20df78c1df794c151c6e96fce60b
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2022-04-09 16:49:47 +0500

    Fetching Data from an API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { getList } from "./services/list";
+
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const mounted = true;
+    getList().then((data) => {
+      if (mounted) {
+        setList(data);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="wrapper">
+      <h1>My Grocery List</h1>
+      <ul>
+        {list.map((item) => (
+          <li key={item.item}>{item.item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
